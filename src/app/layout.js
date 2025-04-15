def5bdce8165a7893276583741d89df7416cc38d
Calebe <shadowhilles@gmail.com>
2025-04-15 18:46:31 -0300

    setup: initial project setup with Next.js, Tailwind, Framer Motion and Shadcn. Base UI configuration

---
+++ src/app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Luterana Biguaçu",
+  description: "Culto todos os domingos 19:30",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-neutral-950`}>
+        <div className="bg-noise"></div>
+        {children}
+      </body>
+    </html>
+  );
+}
+
---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center text-center px-4 text-neutral-100">
+      <h1 className="text-4xl font-bold mb-3">
+        Bem-vindo à Luterana Biguaçu!
+      </h1>
+      <p className="text-xl">
+        Culto todos os domingos às <strong>19:30</strong>.
+      </p>
+    </main>
+  );
+}
